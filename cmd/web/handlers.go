87a4131ea99f07f0878bb7e6518e1c92a02ef9c0
Chris Diebold <chrisdiebold@protonmail.com>
2026-02-22 14:25:24 -0500

    execute base template

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -13,14 +13,20 @@ func home(w http.ResponseWriter, r *http.Request) {
 	// Execute() writes the template content as the response body.
 	// The last arg to Execute() represents dynamic data we want to pass in.
 	w.Header().Add("Server", "Go")
-	ts, err := template.ParseFiles("./ui/html/pages/home.tmpl.html")
+	//base template must be first - every template is compiled into it
+	files := []string{
+		"./ui/html/base.tmpl.html",
+		"./ui/html/pages/home.tmpl.html",
+	}
+
+	ts, err := template.ParseFiles(files...)
 	if err != nil {
 		log.Print(err.Error())
 		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
 		return
 	}
 
-	err = ts.Execute(w, nil)
+	err = ts.ExecuteTemplate(w, "base", nil)
 	if err != nil {
 		log.Print(err.Error())
 		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
