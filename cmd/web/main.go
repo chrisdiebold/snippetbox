673197feba7c5241011fd8adc85e9fa2b5ac23d2
Chris Diebold <chrisdiebold@protonmail.com>
2026-02-22 15:51:46 -0500

    error handling and route encapsulation

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -25,24 +25,14 @@ func main() {
 	app := &application{
 		logger: logger,
 	}
-	// a serveMux is a router. Stores a mapping between URLs routing patterns
-	// for the application and the corresponding handlers (controllers)
-	mux := http.NewServeMux()
+
 	// serve static files such as css, js, and images
-	fileServer := http.FileServer(http.Dir("./ui/static/"))
-	mux.Handle("GET /static/", http.StripPrefix("/static", fileServer))
-	// using {$} to prevent subtree path patterns from acting like they have a
-	// wildcard at the end - ends the pattern
-	mux.HandleFunc("GET /{$}", app.home)
-	mux.HandleFunc("GET /snippet/view/{id}", app.snippetView)
-	mux.HandleFunc("GET /snippet/create", app.snippetCreate)
-	mux.HandleFunc("POST /snippet/create", app.snippetCreatePost)
 
 	logger.Info("starting server", "addr", *addr)
 	// starts a web server. If this returns an err we use the log.Fatal() function to log the
 	// error message and terminate the program.
 	// Note: any error returned by http.ListenAndServe() is always non-nil
-	err := http.ListenAndServe(*addr, mux)
+	err := http.ListenAndServe(*addr, app.routes())
 	logger.Error(err.Error())
 	os.Exit(1)
 }
